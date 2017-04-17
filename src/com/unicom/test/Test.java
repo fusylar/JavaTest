d807e494b85c28ad5e91d5e6971fc9727093dbb3
fusylar <fuyswg@126.com>
2017-04-17 14:33:18 +0800

    test1110

---
+++ src/com/unicom/test/Test.java
@@ -0,0 +1,18 @@
+package com.unicom.test;
+
+/**
+ * Created by FUQIANG on 2017/4/12.
+ */
+public class Test {
+
+
+    public static void main(String []args) {
+
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(2222);
+
+    }
+}
