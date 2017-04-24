5e298c491105877e170757009ff180d033fe8610
fusylar <fuyswg@126.com>
2017-04-24 17:25:39 +0800

    //test2

---
+++ src/com/unicom/test/Test2.java
@@ -0,0 +1,13 @@
+package com.unicom.test;
+
+import sun.applet.Main;
+
+/**
+ * Created by FUQIANG on 2017/4/24.
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
